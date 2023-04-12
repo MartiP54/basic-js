3fc8c6f37bc9f253daa466390813e00f1a819c0a
Marti <marti.iden@gmail.com>
2023-04-13 02:05:53 +0700

    transform

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,10 +13,58 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  let rez = [];
+ if  (typeof arr !== "object") {
+  console.log('no array');
+  throw ( new Error("'arr' parameter must be an instance of the Array!") );
+ }
+
+ for (let i=0; i < arr.length; i++) {
+
+  if (arr[i] === '--discard-prev') {
+    if (i === 0) {
+      rez = rez.concat(arr.slice(1,arr.length));
+    } else {
+    rez = rez.concat(arr.slice(0,i-1), arr.slice(i+1,arr.length));
+    }
+    return rez;
+  }
+  if (arr[i] === '--double-prev') {
+    if (i === 0) {
+      rez = rez.concat(arr.slice(1,arr.length));
+    } else {
+    rez = rez.concat(arr.slice(0,i),arr.slice(i-1,i),arr.slice(i+1,arr.length));
+  }
+  console.log(rez);
+  return rez;
 }
+  if (arr[i] == '--double-next') {
+    if (i === (arr.length-1)) {
+      rez = rez.concat(arr.slice(0,arr.length-1));
+    } else {
+    rez = rez.concat(arr.slice(0,i), arr.slice(i+1,i+2),arr.slice(i+1,arr.length));
+  }
+  return rez;
+}
+
+  if (arr[i] === '--discard-next') {
+    if (i === (arr.length-1)) {
+      rez = rez.concat(arr.slice(0,arr.length-1));
+    } else {
+    rez = rez.concat(arr.slice(0,i),arr.slice(i+2,arr.length));
+  }
+  return rez;
+}
+}
+
+rez = rez.concat(arr);
+console.log(rez);
+return rez;
+
+}
+
+transform([1, 2, 3, '--discard-next', 1337, '--double-prev', 4, 5]);
 
 module.exports = {
   transform
