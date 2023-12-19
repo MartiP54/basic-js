967ca6adb4b17681d49b898d6ca0cc7389cb6c5c
MartiP54 <116718918+MartiP54@users.noreply.github.com>
2023-12-19 14:34:00 +0700

    dns ststs

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let arrDomains = [];
+  let strTemp2 = '';
+  let strTemp1 = '';
+  let rezultObj = {}; 
+
+
+  domains.map((item) => {
+    strTemp1 = item;
+    strTemp2 = '';
+    while(strTemp1.lastIndexOf('.') != -1) {
+      strTemp2 = strTemp2 + strTemp1.slice(strTemp1.lastIndexOf('.')) ;
+      strTemp1 = strTemp1.slice(0, strTemp1.lastIndexOf('.'))
+      arrDomains.push(strTemp2)
+    }
+    strTemp2 = `${strTemp2}.${strTemp1}` ;
+    arrDomains.push(strTemp2)
+  });
+  arrDomains.map((item) => {
+    let k = 0;
+    for (let i = 0; i < arrDomains.length; i++) {
+      if (arrDomains[i] == item) {
+        k = k + 1;
+      }
+      rezultObj[item] = k;
+    }
+  });
+  console.log(rezultObj);
+  return rezultObj;
 }
 
 module.exports = {
