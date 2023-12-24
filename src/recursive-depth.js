0f905807ccdfa1149210544cd1e60acc94ea7940
MartiP54 <116718918+MartiP54@users.noreply.github.com>
2023-12-24 19:00:27 +0700

    recursive-depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -10,39 +10,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * depthCalc.calculateDepth([1, 2, 3, 4, 5]) => 1
  * depthCalc.calculateDepth([1, 2, 3, [4, 5]]) => 2
  * depthCalc.calculateDepth([[[]]]) => 3
- *
-//  */
-// class DepthCalculator {
-  
-//   calculateDepth (arr) {
-//     let k =0;
-//       console.log(arr);
-//       for 
-      
-//       if (Array.isArray(arr)) {
-//          k += 1;
-//          console.log(k)
-//          depthCalc.calculateDepth();
-//       }
-    
-//   }
-// }
+ */
 
-
-// const depthCalc = new DepthCalculator();
-// // depthCalc.calculateDepth([1, 2, 3, 4, 5])
-//  depthCalc.calculateDepth([1, 2, 3, [4, 5]]);
-
-// module.exports = {
-//   DepthCalculator
-// };
+let count = 0;
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    let isArray = arr.some (item => Array.isArray(item));
+    if (isArray) {
+      arr = arr.flat();
+      count = count + 1;
+      this.calculateDepth(arr);
+      return count +1;
+    } else {
+      console.log(count +1);
+      return count + 1;
+    }
   }
 }
+
 
 module.exports = {
   DepthCalculator
 };
+
+const depthCalc = new DepthCalculator();
